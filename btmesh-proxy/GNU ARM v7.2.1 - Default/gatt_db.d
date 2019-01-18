768e134e8087d875188582ead17b47ad0f348190
Hardik Senjaliya <hardyksenjaliya243@gmail.com>
2019-01-18 15:52:12 -0700

    removed duplicated directory

--- btmesh-proxy/GNU ARM v7.2.1 - Default/gatt_db.d
+++
@@ -1,7 +0,0 @@
-gatt_db.o: ../gatt_db.c \
- C:\Users\hardi\SimplicityStudio\v4_workspace\soc-btmesh-proxy\protocol\bluetooth\bt_mesh\inc\common/bg_gattdb_def.h \
- C:\Users\hardi\SimplicityStudio\v4_workspace\soc-btmesh-proxy\protocol\bluetooth\bt_mesh\inc\common/bg_types.h
-
-C:\Users\hardi\SimplicityStudio\v4_workspace\soc-btmesh-proxy\protocol\bluetooth\bt_mesh\inc\common/bg_gattdb_def.h:
-
-C:\Users\hardi\SimplicityStudio\v4_workspace\soc-btmesh-proxy\protocol\bluetooth\bt_mesh\inc\common/bg_types.h:
--- btmesh-proxy/GNU ARM v7.2.1 - Default/protocol/bluetooth/bt_mesh/src/bg_application_properties.d
+++
@@ -1,5 +0,0 @@
-protocol/bluetooth/bt_mesh/src/bg_application_properties.o: \
- ../protocol/bluetooth/bt_mesh/src/bg_application_properties.c \
- C:\Users\hardi\SimplicityStudio\v4_workspace\soc-btmesh-proxy\platform\bootloader\api/application_properties.h
-
-C:\Users\hardi\SimplicityStudio\v4_workspace\soc-btmesh-proxy\platform\bootloader\api/application_properties.h:
